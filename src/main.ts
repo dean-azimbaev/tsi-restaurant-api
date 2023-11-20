50dd34ad171146fca46605f1dbd58ba38255028c
dazimbaev <dean.azimbaev03@gmail.com>
2023-11-20 20:47:01 +0600

    enable swagger

--- src/main.ts
+++ src/main.ts
@@ -3,40 +3,34 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
-import { ConfigService, Environment } from './config';
+import { ConfigService } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const {
     app: { http, host },
-    environment,
     swagger,
+    environment,
   }: ConfigService = app.get<ConfigService>(ConfigService);
 
   app.enableCors();
 
-  if (Environment.DEVELOPMENT === environment) {
-    const config = new DocumentBuilder()
-      .setTitle(swagger.info.title)
-      .setDescription(swagger.info.description)
-      .setVersion(swagger.info.version)
-      .setExternalDoc('Postman Collection', `${host}/${swagger.path}-json`)
-      .build();
+  const config = new DocumentBuilder()
+    .setTitle(swagger.info.title)
+    .setDescription(swagger.info.description)
+    .setVersion(swagger.info.version)
+    .setExternalDoc('Postman Collection', `${host}/${swagger.path}-json`)
+    .build();
 
-    const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config);
 
-    SwaggerModule.setup(swagger.path, app, document);
-  }
+  SwaggerModule.setup(swagger.path, app, document);
 
-
-  await app.startAllMicroservices().catch((e) => {
-    Logger.error(
-      `[app.startAllMicroservices()] WHILE STARTING APP: ${e.message}`,
-    );
-  });
   await app
     .listen(http.port)
-    .then(() => Logger.log(`Service started on http port ${http.port}`));
+    .then(() =>
+      Logger.log(`API started on http port ${http.port}, env: ${environment}`),
+    );
 }
 bootstrap();
